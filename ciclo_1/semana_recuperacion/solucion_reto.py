914c951ff7d3dd6fd4781f235fc80724e8a5c8b7
IsraelArbona <i.arbonammisena.edu.co>
2022-06-13 07:42:18 -0500

    Clase de recuperación 13 junio del 2022

---
+++ ciclo_1/semana_recuperacion/solucion_reto.py
@@ -0,0 +1,17 @@
+def CDT(usuario: str, capital: int, tiempo: int):
+    porcentaje_interes = 0.03
+    porcentaje_aperder = 0.02
+
+
+    if tiempo > 2:
+        valor_interes = (capital * porcentaje_interes * tiempo) / 12
+        valor_total = capital + valor_interes
+    else:
+        valor_aperder = capital * porcentaje_aperder
+        valor_total = capital - valor_aperder
+
+    return 'Para el usuario ' + str(usuario) + ' La cantidad de dinero, según el monto inicial ' \
+           + str(capital) + ' para un tiempo de '+ str(tiempo) + ' meses es: ' + str(valor_total)
+
+
+# print(CDT('AB1012',1000000,1))

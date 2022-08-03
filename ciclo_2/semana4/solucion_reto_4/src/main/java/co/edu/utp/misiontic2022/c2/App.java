b6d9cca148f0b097eb5434e6ceb7d858d03bfd07
IsraelArbona <i.arbonammisena.edu.co>
2022-08-03 10:01:06 -0500

    Clase 3 de agosto del 2022

---
+++ ciclo_2/semana4/solucion_reto_4/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -0,0 +1,23 @@
+package co.edu.utp.misiontic2022.c2;
+
+import co.edu.utp.misiontic2022.c2.view.ReportesView;
+
+/**
+ * Solucion reto 4
+ * jdbc
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /* 
+        var reportesView = new ReportesView();
+        String banco = "Conavi";
+        reportesView.proyectosFinanciadosPorBanco(banco);
+        */
+        
+        var reportesView2 = new ReportesView();
+        var limiteInferior = 50_000d;
+        reportesView2.totalAdeudadoPorProyectosSuperioresALimite(limiteInferior);
+    }
+}
---
+++ ciclo_2/semana4/solucion_reto_4/src/main/java/co/edu/utp/misiontic2022/c2/model/dao/DeudasPorProyectoDao.java
@@ -0,0 +1,61 @@
+package co.edu.utp.misiontic2022.c2.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import co.edu.utp.misiontic2022.c2.model.vo.DeudasPorProyectoVo;
+import co.edu.utp.misiontic2022.c2.util.JDBCUtilities;
+
+public class DeudasPorProyectoDao {
+
+    public List<DeudasPorProyectoVo> listar (Double limite) throws SQLException {
+
+        var respuesta = new ArrayList<DeudasPorProyectoVo>();
+
+        Connection conn = JDBCUtilities.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        String sql = " SELECT p.ID_Proyecto AS ID, SUM(c.Cantidad * mc.Precio_Unidad) AS VALOR " +
+                     " FROM Proyecto p " +
+                     " JOIN Compra c ON (p.ID_Proyecto = c.ID_Proyecto) " +
+                     " JOIN MaterialConstruccion mc ON (c.ID_MaterialConstruccion = mc.ID_MaterialConstruccion) " +
+                     " WHERE c.Pagado = 'No' " +
+                     " GROUP BY p.ID_Proyecto " +
+                     " HAVING VALOR > ? " +
+                     " ORDER BY VALOR DESC;";
+        
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setDouble(1, limite);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                DeudasPorProyectoVo deuda = new DeudasPorProyectoVo();
+
+                deuda.setId(rs.getInt("ID"));
+                deuda.setValor(rs.getDouble("VALOR"));
+
+                respuesta.add(deuda);
+                
+            }
+        } finally {
+            if (rs != null){
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null){
+                conn.close();
+            }
+        }
+        return respuesta;
+        
+    }
+    
+}

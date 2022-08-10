fcbce0f9b983878e173c9755006e1e9c95bbea80
IsraelArbona <i.arbona@misena.edu.co>
2022-08-10 15:32:45 -0500

    Clase 10 de agosto del 2022

--- ciclo_2/semana5/solucion_reto_52/src/main/java/co/edu/utp/misiontic2022/c2/controller/RequerimientoController.java
+++ ciclo_2/semana5/solucion_reto_52/src/main/java/co/edu/utp/misiontic2022/c2/controller/RequerimientoController.java
@@ -1,7 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package co.edu.utp.misiontic2022.c2.controller;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 import co.edu.utp.misiontic2022.c2.model.dao.AsesorPorCiudadDao;
 import co.edu.utp.misiontic2022.c2.model.dao.CompraPorProveedorDao;
@@ -9,28 +10,37 @@ import co.edu.utp.misiontic2022.c2.model.dao.ProyectoCasaCampestreDao;
 import co.edu.utp.misiontic2022.c2.model.vo.AsesorPorCiudadVo;
 import co.edu.utp.misiontic2022.c2.model.vo.CompraPorProveedorVo;
 import co.edu.utp.misiontic2022.c2.model.vo.ProyectoCasaCampestreVo;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+/**
+ *
+ * @author israelarbonaguerrero
+ */
 public class RequerimientoController {
-    
+   
     private final AsesorPorCiudadDao asesorPorCiudadDao;
     private final ProyectoCasaCampestreDao proyectoCasaCampestreDao;
     private final CompraPorProveedorDao compraPorProveedorDao;
 
     public RequerimientoController() {
-        this.asesorPorCiudadDao = new AsesorPorCiudadDao();
-        this.proyectoCasaCampestreDao = new ProyectoCasaCampestreDao();
-        this.compraPorProveedorDao = new CompraPorProveedorDao();
+        asesorPorCiudadDao = new AsesorPorCiudadDao();
+        proyectoCasaCampestreDao = new ProyectoCasaCampestreDao();
+        compraPorProveedorDao = new CompraPorProveedorDao();
     }
+
 
     public ArrayList<AsesorPorCiudadVo> consultarAsesorPorCiudad() throws SQLException{
-        return this.asesorPorCiudadDao.listar();
-    }
-    
-    public ArrayList<ProyectoCasaCampestreVo> consultarProyectoCasaCampestre() throws SQLException{
-        return this.proyectoCasaCampestreDao.listar();
+        return asesorPorCiudadDao.listar();
     }
 
-    public ArrayList<CompraPorProveedorVo> consultarCompraPorProveedor() throws SQLException{
-        return this.compraPorProveedorDao.listar();
+    public ArrayList<ProyectoCasaCampestreVo> consultarProyectoCasaCampestre() throws SQLException {
+        return proyectoCasaCampestreDao.listar();
     }
+
+    public ArrayList<CompraPorProveedorVo> consultarCompraPorProveedor() throws SQLException {
+        return compraPorProveedorDao.listar();
+    }
+
 }
+
